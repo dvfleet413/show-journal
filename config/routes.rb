3b7f346b4cebac11c95618a3d8af869016018d74
github username <github email address>
2020-04-05 11:10:51 -0400

    added resource routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :shows
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :reviews, only: [:index, :show, :new, :create, :edit, :update]
+    resources :shows, only: [:index, :show]
+  end
+  resources :composers
+  resources :genres
 end
