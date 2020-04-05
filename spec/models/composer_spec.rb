a0a6b805beebbd1e09cfefa375aad3658f6ef829
github username <github email address>
2020-04-05 18:00:32 -0400

    apecs for composer validations

--- spec/models/composer_spec.rb
+++ spec/models/composer_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Composer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:composer) {
+    Composer.create(
+      name: "Giuseppe Verdi",
+      birth_year: 1813,
+      death_year: 1901,
+      country: "Italy"
+    )
+  }
+
+  it "is valid with a name, birth_year, death_year, and country" do
+    expect(composer).to be_valid
+  end
+
+  it "is not valid without name, birth_year, death_year, or country" do
+    @composer = Composer.create(
+      birth_year: 1813,
+      death_year: 1901,
+      country: "Italy"
+    )
+    expect(@composer).to_not be_valid
+
+    @composer = Composer.create(
+      name: "Giuseppe Verdi",
+      death_year: 1901,
+      country: "Italy"
+    )
+    expect(@composer).to_not be_valid
+
+    @composer = Composer.create(
+      name: "Giuseppe Verdi",
+      birth_year: 1813,
+      country: "Italy"
+    )
+    expect(@composer).to_not be_valid
+
+    @composer = Composer.create(
+      name: "Giuseppe Verdi",
+      birth_year: 1813,
+      death_year: 1901
+    )
+    expect(@composer).to_not be_valid
+  end
 end
