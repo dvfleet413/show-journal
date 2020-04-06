fec4d0b8049213b092967115261a6e5bb8d5772d
github username <github email address>
2020-04-06 10:47:29 -0400

    specs for Viewing associations

--- spec/models/viewing_spec.rb
+++ spec/models/viewing_spec.rb
@@ -29,10 +29,25 @@ RSpec.describe Viewing, type: :model do
     end
 
     describe "Associations" do
+        before(:each) do
+            @composer = Composer.create(name: "Giuseppe Verdi", birth_year: 1813, death_year: 1901, country: "Italy")
+            @genre = Genre.create(name: "Tragedy")
+            @show = Show.create(title: "Don Carlo", first_performance_year: 1876, composer_id: @composer.id, genre_id: @genre.id)
+            @user = User.create(username: Faker::Internet.username, email: Faker::Internet.email, password: Faker::Internet.password)
+            @viewing = Viewing.create(date: Date.today, location: Faker::Address.city, user_id: @user.id, show_id: @show.id)
+            @review = Review.create(body: "This show was awesome!", rating: 5, viewing_id: @viewing.id)
+        end
 
+        it "belongs_to a user" do
+            expect(@viewing.user).to eq(@user)
+        end
 
+        it "belongs_to a show" do
+            expect(@viewing.show).to eq(@show)
+        end
 
-
-
+        it "has_one review" do
+            expect(@viewing.review).to eq(@review)
+        end
     end
 end
