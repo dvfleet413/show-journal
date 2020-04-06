1df57eb63f38325e28c4b787201f9092489dc3d5
github username <github email address>
2020-04-06 01:09:29 -0400

    doodled some commented out method ideas in user and show models

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,17 @@ class Show < ApplicationRecord
   belongs_to :composer
   belongs_to :genre
   validates_presence_of :title, :first_performance_year
+
+
+#  idea for class level scoped 
+#    def self.from_nineteenth_century
+#      where("first_performance_year >= ? AND first_performance_year < ?", 1800, 1900)
+#    end
+#
+#  I think it can also be written like this...
+#
+#    scope :from_ninteenth_century, -> { where("first_performance_year >= ? AND first_performance_year < ?", 1800, 1900) }
+#
+#  Show.from_nineteenth_century should return all shows first performed between 1800 and 1900
+#  Can do the same with 20th and 21st centrury
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+
+
+
+
+#  Idea for simple model instance method to list all composers that user has seen
+#    def composers
+#      shows.map {|show| show.composer }
+#    end
+#  since user has_many shows through viewings, and each show belongs_to a composer, this should return 
+#  an array of the composers the user has seen
 end
