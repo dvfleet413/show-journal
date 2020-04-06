ee1f3051725af6ccda310ee6299e5af5e507a274
github username <github email address>
2020-04-06 10:34:41 -0400

    added associations for Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :viewing
+  has_one :user, through: :viewing
+  has_one :show, through: :viewing
   validates_presence_of :body, :rating, :viewing_id
   validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5} 
 end
--- app/models/viewing.rb
+++ app/models/viewing.rb
@@ -1,4 +1,5 @@
 class Viewing < ApplicationRecord
   belongs_to :user
+  belongs_to :show
   validates_presence_of :date, :location, :user_id, :show_id
 end
