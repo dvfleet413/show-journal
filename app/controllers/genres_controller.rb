fcccc59f3f15e6cc4bdc77ffb6d0016f7f0c26dc
github username <github email address>
2020-04-06 14:45:33 -0400

    basic functionality of genre#index and genre#show

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,17 @@
+class GenresController < ApplicationController
+
+    def index 
+        @genres = Genre.all
+    end
+
+    def show 
+        set_genre
+    end
+
+
+    private
+        def set_genre
+            @genre = Genre.find(params[:id])
+        end
+
+end
