56c5c0f6df4049961a62f9fbeb251e78072a9e49
github username <github email address>
2020-04-06 10:05:36 -0400

    specs for Viewing validations

--- spec/models/viewing_spec.rb
+++ spec/models/viewing_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Viewing, type: :model do
+    describe "Validations" do
+        before(:each) do 
+            @composer = Composer.create(name: "Giuseppe Verdi", birth_year: 1813, death_year: 1901, country: "Italy")
+            @genre = Genre.create(name: "Tragedy")
+            @show = Show.create(title: "Don Carlo", first_performance_year: 1876, composer_id: @composer.id, genre_id: @genre.id)
+            @user = User.create(username: Faker::Internet.username, email: Faker::Internet.email, password: Faker::Internet.password)
+            @viewing = Viewing.new(date: Date.today, location: Faker::Address.city, user_id: @user.id, show_id: @show.id)
+        end
 
+        it "is valid with a date, location, user_id, and show_id" do
+            expect(@viewing).to be_valid
+        end
+
+        it "is not valid without a date, location, user_id, or show_id" do
+            @timeless_viewing = Viewing.new(date: nil, location: Faker::Address.city, user_id: @user.id, show_id: @show.id)
+            expect(@timeless_viewing).to_not be_valid
+            @homeless_viewing = Viewing.new(date: Date.today, location: nil, user_id: @user.id, show_id: @show.id)
+            expect(@homeless_viewing).to_not be_valid
+            @userless_viewing = Viewing.new(date: Date.today, location: Faker::Address.city, user_id: nil, show_id: @show.id)
+            expect(@userless_viewing).to_not be_valid
+            @showless_viewing = Viewing.new(date: Date.today, location: Faker::Address.city, user_id: @user.id, show_id: nil)
+            expect(@showless_viewing).to_not be_valid 
+        end
+
+
+    end
+
+    describe "Associations" do
+
+
+
+
+
+    end
 end
