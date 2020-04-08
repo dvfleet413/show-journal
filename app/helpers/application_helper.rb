87fc5d102e65fcf2ebc2e22086fc0a6610a42b9b
github username <github email address>
2020-04-08 17:48:56 -0400

    added logged_in? helped method and refactored current_user helper to use memoization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
     def current_user
-        !!session[:user_id] ? User.find(session[:user_id]) : nil
+    #    !!session[:user_id] ? User.find(session[:user_id]) : nil  
+        @user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
     end
 end
