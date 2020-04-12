15834a90a9935a9a6e19148223e133d3b11ef4b8
github username <github email address>
2020-04-11 23:38:28 -0400

    refactoring users_shows page

--- app/helpers/viewings_helper.rb
+++ app/helpers/viewings_helper.rb
@@ -2,4 +2,8 @@ module ViewingsHelper
     def viewing_date(viewing)
         viewing.date.strftime("Viewed on %B %e, %Y")
     end
+
+    def viewing_location(viewing)
+        viewing.location
+    end
 end
